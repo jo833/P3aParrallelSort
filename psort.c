1189344dbb4068d8b788c975c63d6b5e95ead5d3
Julia Oghigian <oghigian@vm-instunix-05.cs.wisc.edu>
2022-11-02 16:40:29 -0500

    some notes from office hours we need to use merge sort instead of odd-even too slow

--- psort.c
+++ psort.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-char input[100];
-int index; //global value
+char input[50000];
+int index; // global value
+
+/*
+ using merge/quick sort instead of odd even
+ use the stack instead heap
+*/
 /*
 current question:
 
     purpose of fsync
     how to determine threads needed
-    using create and join 
+    using create and join
     sort of an idea on how to implement gloabl varibale inuse of odd or even but more info needed
     creating individual test cases
-    
+
 */
-void check_and_swap(void* args)
+void check_and_swap(void *args)
 {
 
     if ((int)input[index] > (int)input[index + 1])
@@ -24,22 +29,25 @@ void check_and_swap(void* args)
     }
 }
 
-void odd_even_sort(char input[], int n)
+void merge_sort(char input[], int n)
 {
     pthread_t odd, even;
-
+    // get threads from anywhere dependendent on processor
     for (int i = 1; i < n; i++)
     {
-        if (i % 2 == 1){
-        // odd case
-        //for loop
-            //call create
-        //for loop
-            //call join 
-        } else {
-        // even case
-        //call create
-        //call join 
+        if (i % 2 == 1)
+        {
+            // odd case
+            // for loop
+            // call create
+            // for loop
+            // call join
+        }
+        else
+        {
+            // even case
+            // call create
+            // call join
         }
         pthread_create(&even, NULL, check_and_swap, NULL);
     }
@@ -48,10 +56,10 @@ void odd_even_sort(char input[], int n)
 int main(int argc, char *argv[])
 {
     FILE *inputFile;
-    //char input[100];
+    // char input[100];
     inputFile = fopen(argv[1], "w+");
     fread(input, 100, 1, inputFile);
-
+    // pull file into heap --> get size -- move to stack
     FILE *outputFile;
     char output[100];
     outputFile = fopen(argv[2], "w+");
@@ -59,7 +67,7 @@ int main(int argc, char *argv[])
     odd_even_sort(input, 100);
 
     fwrite(input, 1, 100, outputFile);
-    //what exactly is the purpose of fsync
+    // what exactly is the purpose of fsync
     fsync();
     fclose(input);
     fclose(output);
