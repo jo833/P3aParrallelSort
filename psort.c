a902d2789af24ff7f899ecc383a6e92bbb00ad46
Julia Oghigian <oghigian@royal-21.cs.wisc.edu>
2022-11-02 21:38:54 -0500

    redoinf of how the file is currently being read and transferred to output file

--- psort.c
+++ psort.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-char input[50000];
+#include <unistd.h>
+int *input;
 int index; // global value
 
 /*
@@ -29,46 +30,70 @@ void check_and_swap(void *args)
     }
 }
 
-void merge_sort(char input[], int n)
+void merge_sort(int *input, int n)
 {
-    pthread_t odd, even;
+    pthread_t first, second;
     // get threads from anywhere dependendent on processor
-    for (int i = 1; i < n; i++)
-    {
-        if (i % 2 == 1)
-        {
-            // odd case
-            // for loop
-            // call create
-            // for loop
-            // call join
-        }
-        else
-        {
-            // even case
-            // call create
-            // call join
-        }
-        pthread_create(&even, NULL, check_and_swap, NULL);
-    }
+    /*  for (int i = 1; i < n; i++)
+      {
+          if (i % 2 == 1)
+          {
+              // odd case
+              // for loop
+              // call create
+              // for loop
+              // call join
+          }
+          else
+          {
+              // even case
+              // call create
+              // call join
+          }
+          pthread_create(&even, NULL, check_and_swap, NULL);
+      }
+      */
 }
 
 int main(int argc, char *argv[])
 {
     FILE *inputFile;
-    // char input[100];
     inputFile = fopen(argv[1], "w+");
-    fread(input, 100, 1, inputFile);
-    // pull file into heap --> get size -- move to stack
+    fseek(inputFile, 0L, SEEK_END);
+    int sizeOfFile = (int)ftell(inputFile);
+    index = sizeOfFile / 4;
+    input = malloc(sizeof *input * sizeOfFile); // probably not right but you get the idea
+    fseek(inputFile, 0, SEEK_SET);
+    fread(input, sizeof(input), 1, inputFile);
+
+    merge_sort(input, index);
     FILE *outputFile;
-    char output[100];
     outputFile = fopen(argv[2], "w+");
+    fwrite(input, sizeof input, 1, outputFile);
+    fclose(inputFile);
+    fclose(outputFile);
+    free(input);
+    // fsync(outputFile);
+    /*   FILE *inputFile;
+       int sizeOfFile;
+       char *fileOnHeap;
+       // char input[100];
+       inputFile = fopen(argv[1], "w+");
 
-    odd_even_sort(input, 100);
+       // pull file into heap --> get size -- move to stack
+       fseek(inputFile, 0L, SEEK_END);
 
-    fwrite(input, 1, 100, outputFile);
-    // what exactly is the purpose of fsync
-    fsync();
-    fclose(input);
-    fclose(output);
+       fread(input, &input, 1, inputFile);
+
+       FILE *outputFile;
+       char output[100];
+       outputFile = fopen(argv[2], "w+");
+
+       odd_even_sort(input, 100);
+
+       fwrite(input, 1, 100, outputFile);
+       // what exactly is the purpose of fsync
+       fsync();
+       fclose(inputFile);
+       fclose(outputFile);*/
 }
