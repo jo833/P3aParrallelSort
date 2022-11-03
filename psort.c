a7b64993ed41d7560dac0cd2fe3cf98c0608cd90
Julia Oghigian <oghigian@royal-03.cs.wisc.edu>
2022-11-03 15:52:16 -0500

    Included merge sort

--- psort.c
+++ psort.c
@@ -2,57 +2,98 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/sysinfo.h>
 int *input;
-int index; // global value
-
+int fileTotalSize;
+int threadTotal;
+int threadCount = 0;
 /*
- using merge/quick sort instead of odd even
- use the stack instead heap
-*/
-/*
-current question:
+    Current Questions:
 
-    purpose of fsync
-    how to determine threads needed
-    using create and join
-    sort of an idea on how to implement gloabl varibale inuse of odd or even but more info needed
-    creating individual test cases
-
+    format of the file
+    correct size of pointer
+    merging at the ende
 */
-void check_and_swap(void *args)
+void combineAllMerge(int lowIndex, int middleIndex, int highIndex)
 {
+    int *leftIndex;
+    int leftIndexSize = middleIndex - lowIndex + 1;
+    leftIndex = malloc(sizeof(int) * (leftIndexSize));
 
-    if ((int)input[index] > (int)input[index + 1])
+    int *rightIndex;
+    int rightIndexSize = highIndex - middleIndex;
+    rightIndex = malloc(sizeof(int) * (rightIndexSize));
+
+    for (int i = 0; i < leftIndexSize; i++)
     {
-        char temp = input[index + 1];
-        input[index + 1] = input[index];
-        input[index] = temp;
+        leftIndex[i] = input[i];
+    }
+    for (int j = 0; j < rightIndexSize; j++)
+    {
+        rightIndex[j] = input[middleIndex + j + 1];
+    }
+
+    int indexer = lowIndex;
+    int leftCounter = 0;
+    int rightCounter = 0;
+
+    while (leftCounter < leftIndexSize && rightCounter < rightIndexSize)
+    {
+        if (rightIndex[rightCounter] >= leftIndex[leftCounter])
+        {
+            input[indexer] = leftIndex[leftCounter];
+            leftCounter++;
+        }
+        else
+        {
+            input[indexer] = rightIndex[rightCounter];
+            rightCounter++;
+        }
+        indexer++;
+    }
+
+    while (leftCounter < leftIndexSize)
+    {
+        input[indexer] = leftIndex[leftCounter];
+        leftCounter++;
+        indexer++;
+    }
+
+    while (rightCounter < rightIndexSize)
+    {
+        input[indexer] = rightIndex[rightCounter];
+        rightCounter++;
+        indexer++;
+    }
+
+    free(rightIndex);
+    free(leftIndex);
+}
+void merge(int lowIndex, int highIndex)
+{
+    int middleIndex = lowIndex + (highIndex + lowIndex) / 2;
+    if (highIndex > lowIndex)
+    {
+        merge(lowIndex, highIndex);
+        merge(middleIndex + 1, highIndex);
+        combineAllMerge(lowIndex, middleIndex, highIndex);
     }
 }
 
-void merge_sort(int *input, int n)
+void *merge_sort(void *args)
 {
-    pthread_t first, second;
-    // get threads from anywhere dependendent on processor
-    /*  for (int i = 1; i < n; i++)
-      {
-          if (i % 2 == 1)
-          {
-              // odd case
-              // for loop
-              // call create
-              // for loop
-              // call join
-          }
-          else
-          {
-              // even case
-              // call create
-              // call join
-          }
-          pthread_create(&even, NULL, check_and_swap, NULL);
-      }
-      */
+    int threadIndex = threadCount;
+    threadCount++;
+    int lowIndex = (threadIndex) * (fileTotalSize / threadTotal);
+    int highIndex = (threadIndex + 1) * (fileTotalSize / threadTotal) - 1;
+    int middleIndex = lowIndex + (highIndex - lowIndex) / 2;
+    if (highIndex > lowIndex)
+    {
+        merge(lowIndex, middleIndex);
+        merge(middleIndex + 1, highIndex);
+        combineAllMerge(lowIndex, middleIndex, highIndex);
+    }
+    return;
 }
 
 int main(int argc, char *argv[])
@@ -61,39 +102,30 @@ int main(int argc, char *argv[])
     inputFile = fopen(argv[1], "w+");
     fseek(inputFile, 0L, SEEK_END);
     int sizeOfFile = (int)ftell(inputFile);
-    index = sizeOfFile / 4;
+    fileTotalSize = sizeOfFile / 4;
     input = malloc(sizeof *input * sizeOfFile); // probably not right but you get the idea
     fseek(inputFile, 0, SEEK_SET);
     fread(input, sizeof(input), 1, inputFile);
+    threadTotal = get_nprocs();
+    pthread_t threads[threadTotal];
+    for (int i = 0; i < threadTotal; i++)
+    {
+        pthread_create(&threads[i], NULL, merge_sort, (void *)NULL);
+    }
+    for (int i = 0; i < threadTotal; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
 
-    merge_sort(input, index);
+    for (int i = 0; i < threadTotal; i++)
+    {
+        combineAllMerge(0, ((i + 1) * (fileTotalSize / threadTotal)) / 2, (i + 1) * (fileTotalSize / threadTotal) - 1);
+    }
+
     FILE *outputFile;
     outputFile = fopen(argv[2], "w+");
     fwrite(input, sizeof input, 1, outputFile);
     fclose(inputFile);
     fclose(outputFile);
     free(input);
-    // fsync(outputFile);
-    /*   FILE *inputFile;
-       int sizeOfFile;
-       char *fileOnHeap;
-       // char input[100];
-       inputFile = fopen(argv[1], "w+");
-
-       // pull file into heap --> get size -- move to stack
-       fseek(inputFile, 0L, SEEK_END);
-
-       fread(input, &input, 1, inputFile);
-
-       FILE *outputFile;
-       char output[100];
-       outputFile = fopen(argv[2], "w+");
-
-       odd_even_sort(input, 100);
-
-       fwrite(input, 1, 100, outputFile);
-       // what exactly is the purpose of fsync
-       fsync();
-       fclose(inputFile);
-       fclose(outputFile);*/
 }
